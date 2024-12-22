acd3ab5f56da190525fc2156d0cb9a33553e1071
belqisk <165401508+belqisk@users.noreply.github.com>
2024-12-23 03:01:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
           description: 'GleamDental-lab is a leading dental laboratory specializing in high-quality, innovative dental solutions. With a focus on precision and craftsmanship, we ensure each restoration is perfectly crafted using the latest technology. Our goal is to restore both function and beauty, enhancing every smile. ', 
           date: '2024', 
           image1: 'path/to/Group 1142814699_5_11zon.webp',
-          image2: 'path/to/Group 1142814699_4_11zon.webp', 
-          image3: 'path/to/Group 1142814699_3_11zon.webp', 
-          image4: 'path/to/Group 1142814699_2_11zon.webp', 
-          image5: 'path/to/Group 1142814699_1_11zon.webp' 
+          image2: 'path/to/Group 1142814700_4_11zon.webp', 
+          image3: 'path/to/Group 1142814701_3_11zon.webp', 
+          image4: 'path/to/Group 1142814702_2_11zon.webp', 
+          image5: 'path/to/Group 1142814703_1_11zon.webp' 
         },
         { category: '3d', 
           title: 'Nestlé x Li-Ning Pop-up Store', 
